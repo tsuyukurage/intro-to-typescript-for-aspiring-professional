fbf72377a0624e2efff6e49afaa392c5eb55f17c
tsuyuri-n <t.suzuki.9657@gmail.com>
2024-06-02 01:26:13 +0900

    1.2.1

--- src/chapter1/chapter1.ts
+++ src/chapter1/chapter1.ts
@@ -8,3 +8,25 @@ function repeatHello(count: number): string {
 
 // 型エラーが消える
 console.log(repeatHello(10));
+
+
+// TypeScriptでは下の関数オーバーローディングはエラーになる
+function doble(value: number) {
+  console.log(value * 2);
+}
+function doble(value: string) {
+  console.log(value.repeat(2));
+}
+
+
+// TypeScriptで上を実装すると
+function double(value: number | string) {
+  if (typeof value === "number") {
+    console.log(value * 2);
+  } else {
+    console.log(value.repeat(2));
+  }
+}
+
+double(123);
+double("hello");
